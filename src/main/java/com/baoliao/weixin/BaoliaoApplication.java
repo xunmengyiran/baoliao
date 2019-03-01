73650747abd1e91da034c0c33443973b44f7e67e
chengchuanqi <cqcheng@akazam.com>
2019-03-01 13:23:05 +0800

    初始化

---
+++ src/main/java/com/baoliao/weixin/BaoliaoApplication.java
@@ -0,0 +1,13 @@
+package com.baoliao.weixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaoliaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaoliaoApplication.class, args);
+    }
+
+}
